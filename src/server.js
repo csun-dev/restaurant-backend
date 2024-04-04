6e1d37688b6edef1e0d2178297d9ed3719fafb83
JongminChoi98 <joeychoi7624@gmail.com>
2024-04-03 17:56:03 -0700

    [ADD] cors config for vite env

--- src/server.js
+++ src/server.js
@@ -10,7 +10,12 @@ export const initServer = () => {
   app.use(morgan("dev"));
   app.use(express.json());
   app.use(cookieParser());
-  app.use(cors());
+  app.use(
+    cors({
+      origin: ["http://localhost:5173"],
+      credentials: true,
+    })
+  );
   app.use(router);
 
   app.use((err, req, res, next) => {
